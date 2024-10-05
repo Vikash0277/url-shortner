aeccd33f152515d29e1af6624f90ddfa3a6b0a5f
Vikash0277 <prasadvikash308@gmail.com>
2024-10-05 11:42:20 +0530

    url-shoertner

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { } from 'react'
+import { ShortUrl } from './components/shortUrl'
+import './App.css'
+
+function App() {
+  return (
+    <div className='grid grid-col-1 '>
+
+      
+       <div>Paste the Url</div>
+       <input type="text" />
+       < ShortUrl/>
+       </div>
+  )
+}
+
+
+export default App
---
+++ frontend/src/components/shortUrl.jsx
@@ -0,0 +1,7 @@
+
+
+export const ShortUrl = ()=>{
+    return <div>
+        <button> Short URL </button>
+    </div>
+}
